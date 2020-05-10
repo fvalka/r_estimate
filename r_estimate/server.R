0a76f0157be0a14e4527dffda4f84a1f8454725d
Fabian Valka <fvalka@vektorraum.com>
2020-05-10 22:08:37 +0200

    Clean up

--- r_estimate/server.R
+++ r_estimate/server.R
@@ -52,7 +52,6 @@ server <- function(input, output) {
     dates_all_plot <- first_plot_date + days(0:(last_date-first_plot_date))
     dates_all_plot_extended <- first_date + days(0:((last_date-first_plot_date)+10))
     
-    R_with_dates <- data.frame("Date" = data$case_incidence$dates[-(1:data$window_size)], "R_median" = data$estimated_R$`Median(R)`)
     cases_with_dates <- data.frame("Date" = data$dates, "Cases" = data$cases, 
                                    "Weekday" = date_to_weekday(data$dates))
     
